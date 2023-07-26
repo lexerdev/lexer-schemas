2b8d1e346513a7ad50700f9e8b72ac5e5cf1756a
Bradley Scarlett <brad.scarlett@lexer.io>
2023-07-26 12:35:21 +1000

    Generate the json schemas when generating docs

--- ops/generate_docs.sh
+++ ops/generate_docs.sh
@@ -5,5 +5,6 @@ set -euxo pipefail
 mkdir -p docs/md/schemas
 mkdir -p docs/html
 
+python lexer_schemas/generate_json_schemas.py
 generate-schema-doc --config template_name=md --config footer_show_time=false "jsonschema/*.jsonschema" docs/md/schemas/
 generate-schema-doc --config template_name=js --config footer_show_time=false "jsonschema/*.jsonschema" docs/html/
